580b1a31ebe18bbd94fa7808a50a2ff8d8fd82a7
Bruna Lima <lbruna886@gmail.com>
2021-11-05 16:44:38 -0300

    implementado o crescimento da cobrinha em mais de 1 unidade por vez

--- snake.py
+++ snake.py
@@ -38,35 +38,37 @@ class Snake():
         self.head.centerx = self.head.centerx + self.orientation[0]*self.headSize
         self.head.centery = self.head.centery + self.orientation[1]*self.headSize
         
-    def addBodypart(self) -> None:
-        newBody = pygame.Rect((0,0), (self.headSize/2, self.headSize/2))
-        reference = self.head
-        if self.bodyList:
-            if len(self.bodyList) > 1: reference = self.bodyList[-2]
-            
-            if self.bodyList[-1].centerx - reference.centerx == 0:
-                offset = self.bodyList[-1].centery - reference.centery
-                addDirection = offset/abs(offset)
+    def addBodypart(self, quantity: int = 1) -> None:
+        for i in range(quantity):
+            newBody = pygame.Rect((0,0), (self.headSize/2, self.headSize/2))
+            reference = self.head
+            if self.bodyList:
+                if len(self.bodyList) > 1: reference = self.bodyList[-2]
+                
+                if self.bodyList[-1].centerx - reference.centerx == 0:
+                    offset = self.bodyList[-1].centery - reference.centery
+                    addDirection = offset/abs(offset)
+                    newBody.center = [
+                        self.bodyList[-1].centerx, 
+                        self.bodyList[-1].centery + addDirection*self.headSize
+                    ]
+                if self.bodyList[-1].centery - reference.centery == 0:
+                    offset = self.bodyList[-1].centerx - reference.centerx
+                    addDirection = offset/abs(offset)
+                    newBody.center = [
+                        self.bodyList[-1].centerx + addDirection*self.headSize, 
+                        self.bodyList[-1].centery
+                    ]
+            else:
                 newBody.center = [
-                    self.bodyList[-1].centerx, 
-                    self.bodyList[-1].centery + addDirection*self.headSize
-                ]
-            if self.bodyList[-1].centery - reference.centery == 0:
-                offset = self.bodyList[-1].centerx - reference.centerx
-                addDirection = offset/abs(offset)
-                newBody.center = [
-                    self.bodyList[-1].centerx + addDirection*self.headSize, 
-                    self.bodyList[-1].centery
-                ]
-        else:
-            newBody.center = [
-                self.head.centerx - self.orientation[0]*self.headSize,
-                self.head.centery - self.orientation[1]*self.headSize
-            ] # first increment depends on direction
-        self.bodyList.append(newBody)
+                    self.head.centerx - self.orientation[0]*self.headSize,
+                    self.head.centery - self.orientation[1]*self.headSize
+                ] # first increment depends on direction
+            self.bodyList.append(newBody)
     
     def changeOrientation(self, orientation: list = None) -> None:
         if orientation:
+            # snake cannot make 180 turn
             selfOpposite = [ -self.orientation[0], -self.orientation[1] ]
             if orientation != selfOpposite:
                 self.orientation = orientation

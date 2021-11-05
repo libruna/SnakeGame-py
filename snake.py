5e906ccb8d0289931dfb30f47201a800bf7bdd94
Bruna Lima <lbruna886@gmail.com>
2021-11-05 18:40:36 -0300

    adicionada a possibilidade de remover pontos da cobrinha

--- snake.py
+++ snake.py
@@ -39,32 +39,36 @@ class Snake():
         self.head.centery = self.head.centery + self.orientation[1]*self.headSize
         
     def addBodypart(self, quantity: int = 1) -> None:
-        for i in range(quantity):
-            newBody = pygame.Rect((0,0), (self.headSize/2, self.headSize/2))
-            reference = self.head
-            if self.bodyList:
-                if len(self.bodyList) > 1: reference = self.bodyList[-2]
-                
-                if self.bodyList[-1].centerx - reference.centerx == 0:
-                    offset = self.bodyList[-1].centery - reference.centery
-                    addDirection = offset/abs(offset)
+        if quantity < 0:
+            for i in range(abs(quantity)):
+                self.bodyList.pop()
+        else:
+            for i in range(quantity):
+                newBody = pygame.Rect((0,0), (self.headSize/2, self.headSize/2))
+                reference = self.head
+                if self.bodyList:
+                    if len(self.bodyList) > 1: reference = self.bodyList[-2]
+                    
+                    if self.bodyList[-1].centerx - reference.centerx == 0:
+                        offset = self.bodyList[-1].centery - reference.centery
+                        addDirection = offset/abs(offset)
+                        newBody.center = [
+                            self.bodyList[-1].centerx, 
+                            self.bodyList[-1].centery + addDirection*self.headSize
+                        ]
+                    if self.bodyList[-1].centery - reference.centery == 0:
+                        offset = self.bodyList[-1].centerx - reference.centerx
+                        addDirection = offset/abs(offset)
+                        newBody.center = [
+                            self.bodyList[-1].centerx + addDirection*self.headSize, 
+                            self.bodyList[-1].centery
+                        ]
+                else:
                     newBody.center = [
-                        self.bodyList[-1].centerx, 
-                        self.bodyList[-1].centery + addDirection*self.headSize
-                    ]
-                if self.bodyList[-1].centery - reference.centery == 0:
-                    offset = self.bodyList[-1].centerx - reference.centerx
-                    addDirection = offset/abs(offset)
-                    newBody.center = [
-                        self.bodyList[-1].centerx + addDirection*self.headSize, 
-                        self.bodyList[-1].centery
-                    ]
-            else:
-                newBody.center = [
-                    self.head.centerx - self.orientation[0]*self.headSize,
-                    self.head.centery - self.orientation[1]*self.headSize
-                ] # first increment depends on direction
-            self.bodyList.append(newBody)
+                        self.head.centerx - self.orientation[0]*self.headSize,
+                        self.head.centery - self.orientation[1]*self.headSize
+                    ] # first increment depends on direction
+                self.bodyList.append(newBody)
     
     def changeOrientation(self, orientation: list = None) -> None:
         if orientation:
